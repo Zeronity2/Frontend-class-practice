0cb905121d3bc3192818e967e9792cee13563e82
Khushi Patel <khushiwonder@gmail.com>
2026-02-27 14:20:43 +0530

    promises in java

---
+++ ptomise.js
@@ -0,0 +1,13 @@
+//promises
+//pending state
+//fulfilled or success
+//rejected
+
+//create a promise
+let foodOrder = new Promise((resolve, rejected)=>{
+    let restaurantOpen = true
+    if(restaurantOpen){
+        
+    }
+
+})
