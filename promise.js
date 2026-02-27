f0d7b3e96c147c5016dc4a553bce578d11bb59b2
Khushi Patel <khushiwonder@gmail.com>
2026-02-27 14:48:18 +0530

    promises in java

---
+++ promise.js
@@ -0,0 +1,36 @@
+//promises
+//pending state
+//fulfilled or success
+//rejected
+
+//create a promise
+let foodOrder = new Promise((resolve, rejected)=>{
+    let restaurantOpen = true
+    if(restaurantOpen){
+        resolve("Food delivered");
+    }else{
+        rejected("restaurant is closed");
+    }
+
+});
+
+//handle the promise
+foodOrder.then(result=>console.log(result))
+.catch(error=>console.log(error))
+
+function checkResult(marks){
+    return new Promise((resolve, rejected)=>{
+        console.log("checking result")
+        setTimeout(()=>{
+            if(marks>40){
+                console.log("student passed!");
+            }else{
+                console.log("student failed!");
+            }
+        },2000);
+        
+    });
+}
+
+checkResult(80).then(message => console.log("message"))
+.catch(error=>console.log("something went wrong"));
