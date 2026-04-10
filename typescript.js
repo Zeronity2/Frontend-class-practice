4d17785bb610d8743852512eb1e0a1cc242a7172
Khushi Patel <khushiwonder@gmail.com>
2026-04-10 13:43:14 +0530

    typescript in javascript

---
+++ typescript.js
@@ -0,0 +1,28 @@
+"use strict";
+//Typescript
+// ts is superset of js
+//add all the types
+//debugging and error finding is easy
+//js
+// let age = 475
+// age = "hii"
+// age: true
+// //typescript
+// let age:number=56
+// age="hkbjs" //error not allowed
+//npm install -g typescript
+//type system in ts
+//what kind of data variable can hold
+let name1 = "test";
+let age = 35;
+let isStudent = true;
+//arr
+let number = [1, 2, 23, 3];
+let names = ["a", "b", "c"];
+//object
+let user = {
+    name: "khushi",
+    age: 50
+};
+//run the ts file
+// tsc filename.ts
---
+++ typescript.ts
@@ -0,0 +1,32 @@
+//Typescript
+// ts is superset of js
+//add all the types
+//debugging and error finding is easy
+//js
+// let age = 475
+// age = "hii"
+// age: true
+
+// //typescript
+// let age:number=56
+// age="hkbjs" //error not allowed
+
+//npm install -g typescript
+//type system in ts
+//what kind of data variable can hold
+let name1 : string = "test"
+let age : number = 35
+let isStudent : boolean=true
+
+//arr
+let number : number[] = [1,2,23,3]
+let names : string[] = ["a","b","c"]
+
+//object
+let user : {name:string , age:number}={
+    name: "khushi",
+    age: 50
+}
+
+//run the ts file
+// tsc filename.ts
