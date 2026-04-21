8863bfc47da6fa13a75c822690d85d204656219e
Khushi Patel <khushiwonder@gmail.com>
2026-04-21 11:14:08 +0530

    type safe object

--- typesafe.ts
+++ typesafe.ts
@@ -6,3 +6,43 @@ let isActive:boolean;
 isActive = true
 
 //object 
+let user:{name:string, age:number, rollNo:number}={
+    name:"kH",
+    age:34,
+    rollNo:34
+}
+
+//data safe model
+type StuAddress={
+    cityName:string
+    pinCode:Number
+}
+
+type Student={
+    id:number
+    name:string
+    course:string
+    age:number
+    isActive:true
+    address:StuAddress
+}
+
+//create a data
+let Student1:Student={
+    id:43,
+    name:"ch",
+    course:"Btech",
+    age:50,
+    isActive:true,
+    address:{
+        cityName:"Varanasi",
+        pinCode:231302
+    }
+}
+
+//print
+function print(Student1:Student){
+    console.log("id is",Student1.id)
+    console.log("name is",Student1.name)
+    //same for all the properties
+}
